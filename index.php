c8bfe6c1a6a77c12e9520eac6e20e6bcd6374d1a
fanny-ra <fanny.rmdnii12@gmail.com>
2025-08-07 20:31:36 +0700

    Modifikasi

--- index.php
+++ index.php
@@ -5,4 +5,10 @@ include("classes/User.php");
 $user = new User("localhost", "root", "", "authoop58");
 var_dump($user->getAll());
 
+echo("Alhamdulillah");
+echo("Alhamdulillah");
+echo("Alhamdulillah");
+echo("Alhamdulillah");
+echo("Alhamdulillah");
+
 ?>
